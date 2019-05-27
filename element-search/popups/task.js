a6a1af128440beb8e2fcacfd418a711cefa296a1
Kirill Bakhtin <flash232323@gmail.com>
2019-05-27 22:06:59 +0600

    btn-success is working now

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,24 +1,29 @@
 'use strict'
 const modalMain = document.getElementById("modal_main"),
       modalClose = document.getElementsByClassName("modal__close"),
-      modalSuccess = document.getElementById("modal_success");
+      modalSuccess = document.getElementById("modal_success"),
+      btnSuccess = document.getElementsByClassName("btn_success");
 
-let modalCloseArray = Array.from(modalClose);
+let modalCloseArray = Array.from(modalClose),
+    btnSuccessArray = Array.from(btnSuccess);
+    
 modalMain.classList.add("modal_active");
-console.log(modalCloseArray)
 
 for (let element of modalCloseArray) {
     element.onclick = function () {
         console.log(modalCloseArray.indexOf(element))
         if (element.className.includes("show-success")) {
             modalMain.classList.remove("modal_active");
-            modalSuccess.classList.add("modal_active");
-            console.log("1")    
+            modalSuccess.classList.add("modal_active");  
         } else {
              modalMain.classList.remove("modal_active");
              modalSuccess.classList.remove("modal_active");
-             console.log("2")
         }
     }
 };
 
+for (let element of btnSuccessArray) {
+    element.onclick = function () {
+        modalSuccess.classList.remove("modal_active");
+    }
+};
